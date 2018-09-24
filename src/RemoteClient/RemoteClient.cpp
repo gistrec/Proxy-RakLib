e71af35e4eedee4a06d9f7a6f814425fff84fe72
gistrec <gistrec@mail.ru>
2018-09-24 23:48:57 +0700

    Добавлена сущность клиента

--- src/RemoteClient/RemoteClient.cpp
+++ src/RemoteClient/RemoteClient.cpp
@@ -1,1 +1,18 @@
 #include "RemoteClient.h"
+
+RemoteClient::RemoteClient(sockaddr_in &address) {
+    // Копируем адрес клиента
+    memcpy(&_address, &address, sizeof(address));
+}
+
+void RemoteClient::handlePacket(Packet &packet) {
+
+}
+
+void RemoteClient::send(Packet &packet) {
+
+}
+
+RemoteClient::~RemoteClient() {
+
+}
--- src/RemoteClient/RemoteClient.h
+++ src/RemoteClient/RemoteClient.h
@@ -1,9 +1,41 @@
 #ifndef MULTIRAKLIB_REMOTECLIENT_H
 #define MULTIRAKLIB_REMOTECLIENT_H
 
+#include "../Packets/Packet.h"
 
 class RemoteClient {
+private:
+    /**
+     * Стадии подключения к раклибу
+     */
+    enum State {connecting, connected, disconnecting, disconnected};
 
+    /**
+     * Стадия подключения к раклибу
+     */
+    State _state = connecting;
+
+    /**
+    * Адрес клиента
+    */
+    struct sockaddr_in _address;
+
+public:
+    explicit RemoteClient(sockaddr_in &address);
+
+    /**
+     * Функция вызывается при получении пакета от игрока
+     * @param packet пакет
+     */
+    void handlePacket(Packet &packet);
+
+    /**
+     * Функция нужна для отправки игроку пакета
+     * @param packet
+     */
+    void send(Packet &packet);
+
+    ~RemoteClient();
 };
 
 
