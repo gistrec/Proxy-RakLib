e3a39ac54b65756ad40b188ee8a523f772d56ffa
gistrec <gistrec@mail.ru>
2018-09-24 21:33:00 +0700

    Релизован класс сокетов

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -1,2 +1,39 @@
 #include "Socket.h"
 
+Socket::Socket(struct sockaddr_in& address) {
+    // Создаем дескриптор сокета
+    _sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (_sockfd == -1) {
+        throw runtime_error("Ошибка при создании сокета");
+    }
+
+    // Связываем дискриптор сокета
+    int result = bind(_sockfd, (struct sockaddr*) &address, sizeof(address));
+    if (result == -1) {
+        cout << "ERROR!" << endl;
+        cout << "IP: " << *inet_ntoa(address.sin_addr) << endl;
+        cout << "Port: " << ntohs(address.sin_port) << endl;
+        throw runtime_error("Ошибка при связывании сокета");
+    }
+
+    // Устанавливаем неблокирующий флаг сокету
+    int flags = fcntl(_sockfd, F_GETFL);
+    flags |= O_NONBLOCK;
+    fcntl(_sockfd, F_SETFL, flags);
+
+    // Копируем адрес сокета
+    memcpy(&_address, &address, sizeof(address));
+}
+
+void Socket::send(const struct sockaddr_in *addr, const char *buf, int buf_size) {
+    sendto(_sockfd, buf, (size_t) buf_size, 0, (struct sockaddr*) addr, sizeof(*addr));
+}
+
+int Socket::read(char *buf, struct sockaddr_in* addr, unsigned int *addr_len) {
+    int recv_len = (int) recvfrom(_sockfd, buf, BUFLEN, 0, (struct sockaddr*) addr, addr_len);
+    return recv_len;
+}
+
+Socket::~Socket() {
+    close(_sockfd);
+}
--- src/Socket.h
+++ src/Socket.h
@@ -1,10 +1,61 @@
 #ifndef MULTIRAKLIB_SOCKET_H
 #define MULTIRAKLIB_SOCKET_H
 
-#include <netinet/in.h>
+#include <arpa/inet.h> // inet_ntoa
+#include <netinet/in.h> // sockaddr_in
+#include <unistd.h> // read(), close()
+#include <fcntl.h>  // fcntl()
+#include <stdexcept> // std::runtime_error
+#include <iostream> // cout, endl
+#include <cstring> // memcpy
+
+#include "Packets/Packet.h"
+#include "RemoteClient/RemoteClient.h"
+
+#define BUFLEN 512
+
+using namespace std;
 
 class Socket {
+private:
+    /**
+     * Дискриптор сокета
+     */
+    int _sockfd;
 
+    /**
+     * Адрес сокета
+     */
+    struct sockaddr_in _address;
+public:
+    /**
+     * Создаем сокет с нужным IP адресом и портом
+     * @param address
+     * @param port
+     */
+    Socket(struct sockaddr_in& address);
+
+    /**
+     * Отправляем пакет
+     * @param addr - адрес
+     * @param buf  - набор байт
+     * @param buf_size - кол-во байт
+     */
+    void send(const struct sockaddr_in *addr, const char *buf, int buf_size);
+
+    /**
+     * Чтение данных из сокета
+     * @param buf  - сюда запишутся данные
+     * @param addr - сюда запишется адрес
+     * @param addr_len - сюда запишется длина адреса
+     * @return int - кол-во байт в принятой датаграмме
+     */
+    int read(char *buf, struct sockaddr_in* addr, unsigned int *addr_len);
+
+    /**
+     * Освобождаем дискриптор сокета
+     */
+    ~Socket();
 };
 
 
